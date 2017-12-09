c7aa1b5bb1eb1dd09c501bd8a40b6be0fe9c1775
taldehyde <taldehyde@gmail.com>
2017-12-09 14:20:39 -0500

    change to imageio

--- src/preprocess_img.py
+++ src/preprocess_img.py
@@ -2,23 +2,10 @@ import numpy as np
 import h5py
 from os.path import join
 import glob
-import scipy.ndimage
-import re
 import natsort
+import imageio
 
-#N = 100
-# all samples
-#N = 18175
 IMG_DIR = '../corpus/faces'
-'''
-def gen_test_vectors(numSample):
-    fs = h5py.File('../corpus/data2_vectors.hdf5', 'r')
-    src = fs['vectors']
-    fd = h5py.File('../corpus/train_vectors.hdf5', 'w')
-    fd.create_dataset('vectors', data=src[:numSample])
-    fs.close()
-    fd.close()
-'''
 
 def gen_train_imgs():
     img_file_list = glob.glob(join(IMG_DIR, '*.jpg'))
@@ -28,8 +15,8 @@ def gen_train_imgs():
     for i,img_file in enumerate(img_file_list):
         if i%100 == 0:
             print('Processing', img_file)
-        img = scipy.ndimage.imread(img_file)
-        img_data[i,:] = img.transpose(2, 0, 1)
+        img = imageio.imread(img_file)
+        img_data[i,...] = img.transpose(2, 0, 1)
 
     with h5py.File('../corpus/train_imgs.hdf5', 'w') as f:
         f.create_dataset('imgs', data=img_data)
